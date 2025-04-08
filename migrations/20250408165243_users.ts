16c6a5bb686c96b403f335f81a2864b85f2f2eff
Abdul-Basit <basitsaleem282@gmail.com>
2025-04-08 22:56:45 +0530

    update in migration file

--- migrations/20250408165243_users.ts
+++ migrations/20250408165243_users.ts
@@ -1,8 +1,8 @@
 import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTableIfNotExists('user', function (table) {
-    table.increments();
+  await knex.schema.createTable('user', function (table) {
+    table.increments('id');
     table.string('name');
     table.integer('age');
     table.string('email').unique();
@@ -11,5 +11,5 @@ export async function up(knex: Knex): Promise<void> {
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTableIfExists('users');
+  await knex.schema.dropTable('user');
 }
